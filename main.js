60e8563a71211d9d4c5205260e4f101f4e90e2fd
unknown <Emmanuel@Anonymouse>
2020-12-07 15:05:00 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function show(){
+  let menu = document.querySelector(".nav-mobile");
+  menu.classList.toggle("toggleCls");
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
